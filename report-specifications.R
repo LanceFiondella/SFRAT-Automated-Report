1eda96bb6d1ab6c36a967fd3ee252bb2e4cb6e7d
vnagaraju14 <vnagaraju@umassd.edu>
2019-01-22 12:29:23 -0500

    Removed subset option and revised code
    Revised the code and removed unnecessary lines

--- report-specifications.R
+++ report-specifications.R
@@ -1,48 +1,31 @@
+############################### EDIT BELOW FOR SCRIPT PARAMETERS ############################### 
 
-# edit below for script parameters #
-	
-#subset data into parts? 1 for yes, 0 for no
-subset_data <- 0
+verbose_report <- 1		#1 for verbose report, 2 for non-verbose
 
-#1 for verbose report, 2 for non-verbose
-verbose_report <- 2									
-
-#select which sheet to pick, in this case gives SYS1
-sheetNumber <- 1
-#designate location of data file
+#Specify the location to the input data file and specific sheet number
 filePath <- '/SFRAT/model_testing/model_data.xlsx'
+sheetNumber <- 1
+colors <- c("navy","red","green","firebrick4","magenta")   # Specify colors to view results of each model
+
 
 #Tab 1 Parameters:
-	#laplace test confidence level
-	confidence_lvl <- 0.9
+confidence_lvl <- 0.9  #Laplace test confidence level
 
 #Tab 2 Parameters:
-	#number of future failures to predict
-	num_failures_future_prediction <- 2	
-
-	#pick models to include, by default is all
-	models_to_apply <- c('DSS', 'GM', 'Wei','GO','JM')
-	#mission time to compute reliability growth
-	mission_time <- 600
+num_failures_future_prediction <- 2	 #Number of future failures to predict
+models_to_apply <- c('DSS', 'GM', 'Wei','GO','JM') #Pick models to include, by default is all
+mission_time <- 600   #Mission time to compute reliability growth
 
 #Tab 3 Parameters:
-	#number of future failures to predict
-	num_failures_to_predict <- 3
-
-	#future prediction time
-	additional_time_software_will_run <- 4116
-
-	#between 0-1, desired software reliability
-	desired_reliability <- .9
-
-	#interval size
-	reliability_interval_length<- 600
+num_failures_to_predict <- 3  #Number of future failures to predict
+additional_time_software_will_run <- 4116  #Future prediction time
+desired_reliability <- .9     #Between 0-1, desired software reliability
+reliability_interval_length<- 600   #Interval size
 
 #Tab 4 Parameters:
-	#predictive sum of squares, percentage
-	percent_data_for_PSSE <- .9
+percent_data_for_PSSE <- .9  #Predictive sum of squares, percentage
 
-#do not edit below --------------------------------------------------------------------------------------#
+############################### DO NOT EDIT BELOW #####################################
 
 library("knitr")
 library("markdown")
@@ -56,24 +39,30 @@ library("ggplot2")
 library("rootSolve")
 
 datapath <- paste0(getwd(), filePath)
-datasettemp <- read_excel(datapath,sheet = sheetNumber)  #Specify the sheet with the data
-SheetName<-excel_sheets(path=datapath)[sheetNumber] #Specify the sheet selected in the above line
-colors <- c("navy","red","green","firebrick4","magenta")
+datasettemp <- read_excel(datapath,sheet = sheetNumber) 
+SheetName<-excel_sheets(path=datapath)[sheetNumber] 
 
 dataset <- datasettemp[1:floor(dim(datasettemp)[1]),]
 x <- verbose_report
 rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),output_file =  paste("SFRAT report_", SheetName,'_', Sys.Date(), ".pdf", sep=''),output_dir = './Reports') 
 
-if (subset_data==1) {
-  ds <- readline(prompt="How many percent of data should be used in each subset? (Input should be between 0 to 1)___")
-  ds <- as.double(ds) 
-  dsi <- ds
-  while(dsi<=1){
-    dataset <- datasettemp[1:floor(dsi*dim(datasettemp)[1]),]
-    rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),output_file =  paste("SFRAT report_", SheetName,'_',dsi,'_', Sys.Date(), ".pdf", sep=''), output_dir = './Reports') 
-    dsi=dsi+ds;
-  }
-  } else {
-  dataset <- read_excel(datapath,sheet = 1)
-  rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'), output_file =  paste("SFRAT report_", SheetName,'_', Sys.Date(), ".pdf", sep=''),output_dir = './Reports') 
-}
+#SUBSET DATA OPTION - FUTURE WORK
+
+#subset data into parts? 1 for yes, 0 for no
+# subset_data <- 0
+# 
+# 
+# if (subset_data==1) {
+#   ds <- readline(prompt="How many percent of data should be used in each subset? (Input should be between 0 to 1)___")
+#   ds <- as.double(ds) 
+#   dsi <- ds
+#   while(dsi<=1){
+#     dataset <- datasettemp[1:floor(dsi*dim(datasettemp)[1]),]
+#     rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'),output_file =  paste("SFRAT report_", SheetName,'_',dsi,'_', Sys.Date(), ".pdf", sep=''), output_dir = './Reports') 
+#     dsi=dsi+ds;
+#   }
+#   } else {
+#   dataset <- read_excel(datapath,sheet = 1)
+#   rmarkdown::render(paste0(getwd(),'/SFRATReport.Rmd'), output_file =  paste("SFRAT report_", SheetName,'_', Sys.Date(), ".pdf", sep=''),output_dir = './Reports') 
+#   }
+
